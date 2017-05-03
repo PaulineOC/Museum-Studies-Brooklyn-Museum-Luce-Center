0c127271d45fdd73ee2cba5c1963d5890441fe22
PaulineOC <poc215@nyu.edu>
2017-05-03 13:46:42 -0400

    hi

--- app.js
+++ app.js
@@ -43,6 +43,11 @@ app.post('/mapSearch',function(req,res){
 				console.log(err);
 			}
 			else{
+
+
+				obj.forEach(function(ele){
+					console.log(ele.title);
+				});
 			res.render('Search', {'searchTerm': obj, "location":req.body.caseNum });
 			}
 		});
@@ -53,14 +58,26 @@ app.get('/AdvancedSearch', function(req, res) {
 	res.render('AdvancedSearch');
 });
 
+
+function advancedSearch(term, obj){
+
+	//32.830
+
+
+}
 app.post('/AdvancedSearch', function(req,res){
+
+	// { $or: [,{"maker":req.body.maker},{"manufacturer":req.body.manufacturer},{"title":req.body.title},{"date":req.body.date},{"medium":req.body.medium},{"location":req.body.location},{'furtherKeywords': { $in: keys}}]}
 	if(req.body){
 		var keys=[];
 		if(req.body.keywords){
 			keys = req.body.keywords.split(',');
 		}
-		museumObj.find({ $or: [{ "accessionNum": req.body.accession},{"maker":req.body.maker},{"manufacturer":req.body.manufacturer},{"title":req.body.title},{"date":req.body.date},{"medium":req.body.medium},{"location":req.body.location},{'furtherKeywords': { $in: keys}}]},function(err,obj){
+		museumObj.find({ "accessionNum": req.body.accession},function(err,obj){
 			if(obj){
+				obj.forEach(function(ele){
+					console.log(ele.title);
+				});
 				res.render('Search', {'searchTerm': obj, "advancedterm": req.body});
 			}
 			else{
@@ -70,6 +87,9 @@ app.post('/AdvancedSearch', function(req,res){
 	}
 });
 
+app.get('/About', function(req,res){
+	res.redirect('https://www.brooklynmuseum.org/opencollection/research/luce/general');
+});
 app.get('/AskUs', function(req, res) {
 	res.redirect('https://www.brooklynmuseum.org/ask');
 });
@@ -79,12 +99,50 @@ app.get('/Themes', function(req, res) {
 });
 
 
-app.get('/search', function(req,res){
-	//console.log(req.query.search);
-	museumObj.find({ $or: [{ "accessionNum":req.query.search},{"maker":req.query.search},{"manufacturer":req.query.search},{"title":req.query.search},{"date":req.query.search},{"medium":req.query.search},{"location":req.query.search},{'furtherKeywords': req.query.search}]}, function(err, obj, count) {
-		res.render('Search', {'searchTerm': obj,"generalterm":req.query.search});
+
+function generalSearch(obj, word){
+	word=word.trim().toLowerCase();
+	console.log(word);
+	console.log(obj.title);
+
+	if(obj.title.includes(word)){
+		console.log('title match');
+		return true;
+	}
+	else if(subjectSearch(obj,word)){
+		console.log("subject matc");
+		return true;
+	}
+
+	else{
+		return false;
+	}
+}
+
+
+app.post('/searchbar',function(req,res){
+
+	console.log(req.body);
+	var fuzzy = new RegExp(".*"+req.body.search.toLowerCase().trim()+".*");
+	 var results=[];
+
+	museumObj.find({ $or: [{ "title": fuzzy},{"subject": { $in: [req.body.search.toLowerCase()]}},{"photographer": fuzzy}, {"identifier": fuzzy}, {"department": fuzzy}, {"collection": fuzzy}, {"repository": fuzzy}, {"coverage": fuzzy}]}, function(err, obj, count) {
+		res.render('Search', {'searchTerm': obj, "generalterm":req.body.search});
 	});
 });
+
+
+// app.get('/search', function(req,res){
+// 	console.log(req.query.search);
+// 	var fuzzy = new RegExp(".*"+req.query.search.toLowerCase().trim()+".*");
+// 	console.log(fuzzy);
+// 	 var results=[];
+// 	 museumObj.find({ $or: [{ "title": fuzzy},{"subject": { $in: [req.query.search.toLowerCase().trim()]}},{ "accessionNum":fuzzy}, {"manufacturer": fuzzy}, {"date": fuzzy}, {"medium": fuzzy}, {"repository": fuzzy}, {"location": fuzzy}]}, function(err, obj, count) {
+// 		res.render('Search', {'searchTerm': results, "generalterm":req.query.search.toLowerCase().trim()});
+// 	});
+
+
+// });
 
 
 var subjectSearch = function(searchObj, term) {
